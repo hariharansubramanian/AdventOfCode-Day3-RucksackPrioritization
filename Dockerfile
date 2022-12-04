7256e7d7e72c5ff05e9c6b7356a327d80779e17a
Hariharan Subramanian <hari@flowinsights.com>
2022-12-04 15:17:27 +0100

    Refactored logic to handle any group count

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["RucksackPrioritization.csproj", "./"]
+RUN dotnet restore "RucksackPrioritization.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "RucksackPrioritization.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "RucksackPrioritization.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "RucksackPrioritization.dll"]
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Scanning rucksacks...");
+
+var priorityDict = new Dictionary<char, int>();
+for (var c = 'a'; c <= 'z'; c++) priorityDict.Add(c, c - 'a' + 1); // 1-26
+for (var c = 'A'; c <= 'Z'; c++) priorityDict.Add(c, c - 'A' + 27); // 27-52
+
+var inventories = File.ReadLines("inventory_list.txt").ToList();
+
+var totalPriorityValue = 0;
+const int groupCount = 3;
+
+inventories
+    .Select((inv, idx) => new {inv, idx})
+    .GroupBy(g => g.idx / groupCount, i => i.inv)
+    .ToList()
+    .ForEach(g =>
+    {
+        var groupInventoryList = g.ToList();
+        var commonInGroup = groupInventoryList.Aggregate((s1, s2) => new string(s1.Intersect(s2).ToArray()));
+        var commonPrioritySum = commonInGroup.Sum(c => priorityDict[c]);
+        totalPriorityValue += commonPrioritySum;
+    });
+
+Console.WriteLine($"Common rucksack items have a total priority of {totalPriorityValue}");
